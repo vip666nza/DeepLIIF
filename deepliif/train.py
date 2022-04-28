48844ca29a8824ad9a9d51451656630120713bd3
Zeeon.N <92867873+vip666nza@users.noreply.github.com>
2022-04-28 15:05:31 +0800

    Update train.py

--- deepliif/train.py
+++ deepliif/train.py
@@ -162,7 +162,7 @@ def train(dataroot, name, gpu_ids, checkpoints_dir, targets_no, input_nc, output
     This script works for various models (with option '--model': e.g., DeepLIIF) and
     different datasets (with option '--dataset_mode': e.g., aligned, unaligned, single, colorization).
     You need to specify the dataset ('--dataroot'), experiment name ('--name'), and model ('--model').
-
+    # by zeeon: 为什么没有看到有传入--dataset_mode这个参数呢？？？？？ 确实在basy_options.py有所定义，但该文件如何能接收到basy_options.py中的定义呢？
     It first creates model, dataset, and visualizer given the option.
     It then does standard network training. During the training, it also visualize/save the images, print/save the loss
     plot, and save models.The script supports continue/resume training.
@@ -199,7 +199,6 @@ def train(dataroot, name, gpu_ids, checkpoints_dir, targets_no, input_nc, output
     # __getitem__返回的格式 {'A': tensor, 'B': [B1_tensor, B2_tensor, ...]}
     
     dataset = create_dataset(dataset, batch_size, serial_batches, num_threads, max_dataset_size, gpu_ids) 
-    # 不明白把dataset这个对象传进去是什么意思，是作为参数dataset_mode吗？
     
     # get the number of images in the dataset.
     click.echo('The number of training images = %d' % len(dataset))
